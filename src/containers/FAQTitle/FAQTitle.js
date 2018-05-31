88bc28de7c92079090f7827279978e55470598af
jraleman <jraleman@bendiburg.com>
2018-05-31 06:15:22 -0700

    Add container FAQTitle

---
+++ src/containers/FAQTitle/FAQTitle.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class FAQTitle extends React.Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <h2 className="title-uppercase text-white">
+            { "Frequently Asked Questions" }
+          </h2>
+        </div>
+      </div>
+    );
+  }
+};
