3296a9bc7780ad231e4c1c33f1fc1a3885b194c0
jraleman <jraleman@bendiburg.com>
2018-06-06 02:30:10 -0700

    Restructure App's render function

--- src/App.js
+++ src/App.js
@@ -5,15 +5,27 @@ import Facilities from './sections/Facilities';
 import FAQ from './sections/FAQ';
 import Contact from './sections/Contact';
 
+import Modal from './components/Modal';
+import Navbar from './components/Navbar';
+import SidebarMenu from './components/SidebarMenu';
+import Toolbar from './components/Toolbar';
+
 class App extends React.Component {
   render () {
     return (
-      <div className="pagepiling">
-        <Banner />
-        <Facilities />
-        <FAQ />
-        <Contact />
-      </div>
+      <section id="pagepiling" className="animsition" style={{ zIndex: 4242 }}>
+        <div className="click-capture"></div>
+        <Modal />
+        <Navbar />
+        <SidebarMenu />
+        <Toolbar />
+        <div className="pagepiling">
+          <Banner />
+          <Facilities />
+          <FAQ />
+          <Contact />
+        </div>
+      </section>
     );
   }
 }
